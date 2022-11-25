f31d2a388b701da980d7558b98cac1027f58d061
Shmuel Zeigerman <solomuz0@gmail.com>
2022-11-25 18:18:21 +0200

    Refactoring

--- luafar/src/farhost.cpp
+++ luafar/src/farhost.cpp
@@ -23,7 +23,8 @@ const char* GetStringFromTable(lua_State *L, const char *Key)
 }
 
 
-// input table is on stack top (-1)
+// - input table is on stack top (-1)
+// - no collector is required
 void FillAnsiPluginPanelItem (lua_State *L, oldfar::PluginPanelItem *pi)
 {
   memset(pi, 0, sizeof(*pi));
@@ -59,33 +60,67 @@ void FillAnsiPluginPanelItem (lua_State *L, oldfar::PluginPanelItem *pi)
 }
 
 
-HMODULE GetPluginModuleHandle(lua_State *L, const PSInfo *psInfo, HANDLE plug_handle, int *ansi)
+struct CommonParams
 {
-  HMODULE dll_handle = NULL;
-  size_t size = psInfo->PluginsControlV3(plug_handle, PCTL_GETPLUGININFORMATION, 0, NULL);
-  if (size != 0)
+  HANDLE hPanel;
+  HMODULE hModule;
+  bool Ansi;
+};
+
+struct ModuleGuard
+{
+  HMODULE hModule;
+  ModuleGuard(HMODULE hm) : hModule(hm) {}
+  ~ModuleGuard() { dlclose(hModule); }
+};
+
+bool GetCommonParams(lua_State *L, CommonParams *Params)
+{
+  HANDLE panHandle = OptHandle(L);   //1-st argument
+  if (panHandle == NULL)
+    return false;
+
+  // get plugin handle (for obtaining dll name)
+  PSInfo *psInfo = GetPluginStartupInfo(L);
+  PanelInfo panInfo;
+  if (! (psInfo->Control(panHandle,FCTL_GETPANELINFO,0,(LONG_PTR)&panInfo) && panInfo.PluginHandle) )
+    return false;
+
+  // get panel handle (a parameter to dll function)
+  psInfo->Control(panHandle, FCTL_GETPANELPLUGINHANDLE, 0, (LONG_PTR)&Params->hPanel);
+  if (Params->hPanel == INVALID_HANDLE_VALUE)
+    return false;
+
+  size_t size = psInfo->PluginsControlV3(panInfo.PluginHandle, PCTL_GETPLUGININFORMATION, 0, NULL);
+  if (size == 0)
+    return false;
+
+  FarGetPluginInformation *piInfo = (FarGetPluginInformation *) malloc(size);
+  if (piInfo == NULL)
+    return false;
+
+  piInfo->StructSize = sizeof(*piInfo);
+  if (0 == psInfo->PluginsControlV3(panInfo.PluginHandle, PCTL_GETPLUGININFORMATION, size, piInfo))
   {
-    struct FarGetPluginInformation *piInfo = (struct FarGetPluginInformation *) malloc(size);
-    if (piInfo != NULL)
-    {
-      piInfo->StructSize = sizeof(*piInfo);
-      if (psInfo->PluginsControlV3(plug_handle, PCTL_GETPLUGININFORMATION, size, piInfo))
-      {
-        *ansi = piInfo->Flags & FPF_ANSI;
-        push_utf8_string(L, piInfo->ModuleName, -1);
-        dll_handle = dlopen(lua_tostring(L,-1), RTLD_NOW | RTLD_GLOBAL); //not to forget to dlclose later
-        lua_pop(L, 1);
-      }
-      free(piInfo);
-    }
+    free(piInfo);
+    return false;
   }
-  return dll_handle;
+
+  Params->Ansi = piInfo->Flags & FPF_ANSI;
+  push_utf8_string(L, piInfo->ModuleName, -1);
+  Params->hModule = dlopen(lua_tostring(L,-1), RTLD_NOW | RTLD_GLOBAL);
+  lua_pop(L, 1);
+  free(piInfo);
+
+  return (Params->hModule != NULL);
 }
 
 
-int _GetFilesW(lua_State *L, HANDLE hPanel, HMODULE dll_handle)
+// result = far.Host.GetFiles (handle, Items, Move, DestPath [, OpMode])
+int _GetFilesW(lua_State *L, HANDLE hPanel, HMODULE hModule)
 {
   typedef int (WINAPI * T_GetFilesW)(HANDLE, struct PluginPanelItem*, int, int, const wchar_t**, int);
+  ModuleGuard mg(hModule);
 
   luaL_checktype(L, 2, LUA_TTABLE);  //2-nd argument
   size_t numLines = lua_objlen(L, 2);
@@ -94,18 +129,13 @@ int _GetFilesW(lua_State *L, HANDLE hPanel, HMODULE dll_handle)
   int OpMode = luaL_optinteger(L, 5, OPM_FIND|OPM_SILENT); //5-th argument
 
   T_GetFilesW getfilesW;
-  if (NULL == (getfilesW = (T_GetFilesW) dlsym(dll_handle, "GetFilesW"))) {
-    dlclose(dll_handle);
+  if (NULL == (getfilesW = (T_GetFilesW) dlsym(hModule, "GetFilesW")))
     return FALSE;
-  }
 
   PluginPanelItem *ppi, *ppi_curr;
   ppi = ppi_curr = (PluginPanelItem*)malloc(sizeof(PluginPanelItem) * numLines);
   if (ppi == NULL)
-  {
-    dlclose(dll_handle);
     return FALSE;
-  }
 
   lua_newtable(L); //collector
   for (size_t i=1; i<=numLines; i++)
@@ -123,14 +153,14 @@ int _GetFilesW(lua_State *L, HANDLE hPanel, HMODULE dll_handle)
   for(; ppi<ppi_curr; ppi++)
     free((void*)ppi->CustomColumnData);
   free(ppi);
-  dlclose(dll_handle);
   return ret;
 }
 
 
-int _GetFilesA(lua_State *L, HANDLE hPanel, HMODULE dll_handle)
+int _GetFilesA(lua_State *L, HANDLE hPanel, HMODULE hModule)
 {
   typedef int (WINAPI * T_GetFilesA)(HANDLE, oldfar::PluginPanelItem*, int, int, char*, int);
+  ModuleGuard mg(hModule);
 
   luaL_checktype(L, 2, LUA_TTABLE);  //2-nd argument
   size_t numLines = lua_objlen(L, 2);
@@ -144,18 +174,13 @@ int _GetFilesA(lua_State *L, HANDLE hPanel, HMODULE dll_handle)
   int OpMode = luaL_optinteger(L, 5, oldfar::OPM_FIND|oldfar::OPM_SILENT); //5-th argument
 
   T_GetFilesA getfilesA;
-  if (NULL == (getfilesA = (T_GetFilesA) dlsym(dll_handle, "GetFiles"))) {
-    dlclose(dll_handle);
+  if (NULL == (getfilesA = (T_GetFilesA) dlsym(hModule, "GetFiles")))
     return FALSE;
-  }
 
   oldfar::PluginPanelItem *ppi, *ppi_curr;
   ppi = ppi_curr = (oldfar::PluginPanelItem*)malloc(sizeof(oldfar::PluginPanelItem) * numLines);
   if (ppi == NULL)
-  {
-    dlclose(dll_handle);
     return FALSE;
-  }
 
   // collector table not needed for the ANSI variant
   for (size_t i=1; i<=numLines; i++)
@@ -173,35 +198,18 @@ int _GetFilesA(lua_State *L, HANDLE hPanel, HMODULE dll_handle)
   for(; ppi<ppi_curr; ppi++)
     free((void*)ppi->CustomColumnData);
   free(ppi);
-  dlclose(dll_handle);
   return ret;
 }
 
 
-// result = far.Host.GetFiles (handle, Items, Move, DestPath [, OpMode])
 extern "C" int far_host_GetFiles(lua_State *L)
 {
-  PSInfo *psInfo = GetPluginStartupInfo(L);
-  HANDLE panHandle = OptHandle(L);   //1-st argument
+  CommonParams CP;
+  if (GetCommonParams(L, &CP))
+    lua_pushinteger(L, (CP.Ansi ? _GetFilesA : _GetFilesW)(L, CP.hPanel, CP.hModule));
+  else
+    lua_pushinteger(L,0);
 
-  // get plugin handle (for obtaining dll name)
-  PanelInfo panInfo;
-  if (! (panHandle && psInfo->Control(panHandle,FCTL_GETPANELINFO,0,(LONG_PTR)&panInfo) && panInfo.PluginHandle) )
-    return lua_pushinteger(L,0), 1;
-
-  // get panel handle (a parameter to dll function)
-  HANDLE hPanel;
-  psInfo->Control(panHandle, FCTL_GETPANELPLUGINHANDLE, 0, (LONG_PTR)&hPanel);
-  if (hPanel == INVALID_HANDLE_VALUE)
-    return lua_pushinteger(L,0), 1;
-
-  // get dll handle (for obtaining dll function address)
-  HMODULE dll_handle;
-  int ansi;
-  if (NULL == (dll_handle = GetPluginModuleHandle(L, psInfo, panInfo.PluginHandle, &ansi)))
-    return lua_pushinteger(L,0), 1;
-
-  lua_pushinteger(L, (ansi ? _GetFilesA : _GetFilesW)(L, hPanel, dll_handle));
   return 1;
 }
 
