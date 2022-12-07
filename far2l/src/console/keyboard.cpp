shmuz/far2m

commit ea09826644a588518b282ca4944fd5421fafbeba

Fix xlats.ini

far2l/src/console/keyboard.cpp
@@ -1795,8 +1795,8 @@ int TranslateKeyToVK(int Key,int &VirtKey,int &ControlState,INPUT_RECORD *Rec)
 			VirtKey=FKey;
 		else if ((unsigned int)FKey > KEY_FKEY_BEGIN && (unsigned int)FKey < KEY_END_FKEY)
 			VirtKey=FKey-KEY_FKEY_BEGIN;
-		else if (FKey < MAX_VKEY_CODE)
-			VirtKey=WINPORT(VkKeyScan)(FKey);
+		//else if (FKey < MAX_VKEY_CODE)
+		//	VirtKey=WINPORT(VkKeyScan)(FKey);
 		else
 			VirtKey=FKey;
 	}
@@ -2875,7 +2875,7 @@ DWORD CalcKeyCode(INPUT_RECORD *rec,int RealKey,int *NotMacros)
 		{
 			if (!Opt.ShiftsKeyRules || WaitInFastFind > 0)
 				return KEY_ALT|Upper(Char);
-			else if (WaitInMainLoop)
+			else // if (WaitInMainLoop)
 				return KEY_ALT|Char;
 		}
 

far2l/bootstrap/xlats.ini
@@ -10,10 +10,10 @@ Latin=#FDULTPBQRKVYJGHCNEA{WXIO}SMZfdultpbqrkvyjghcnea[wxio]sm'z`~<>
 Local=№АВГДЕЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЯавгдезийклмнопрстуфхцчшщъыьэяёЁБЮ
 
 ; extra translation pairs used if last language-specific character was latin
-AfterLatin=,? ?& ./ б, ю. :^ Ж: ж; ;$ \" @Э \"
+AfterLatin=?, &? /. ,б .ю ^: :Ж ;ж $; @" "Э
 
 ; extra translation pairs used if last language-specific character was local
-AfterLocal=?, &? /. ,б .ю ^: :Ж ;ж $; @\ "\ "Э
+AfterLocal=,? ?& ./ б, ю. :^ Ж: ж; ;$ "@ Э"
 
 ; extra translation pairs used if there were no language-specific characters yet
-AfterOther=^: :Ж Ж^ $; ;ж ж$ @\ "\ "Э Э@ &? ?, ,б б& /. .ю ю/
+AfterOther=^: :Ж Ж^ $; ;ж ж$ @" "Э Э@ &? ?, ,б б& /. .ю ю/

luafar/src/service.c
@@ -4841,14 +4841,12 @@ int far_UnloadPlugin(lua_State *L)     { return plugin_load(L, PCTL_UNLOADPLUGIN
 int far_XLat (lua_State *L)
 {
   size_t size;
-  wchar_t *Line = check_utf8_string(L, 1, &size), *str;
-  intptr_t StartPos = luaL_optinteger(L, 2, 1) - 1;
-  intptr_t EndPos = luaL_optinteger(L, 3, size);
+  wchar_t *Line = check_utf8_string(L, 1, &size);
+  int StartPos = luaL_optinteger(L, 2, 1) - 1;
+  int EndPos = luaL_optinteger(L, 3, size);
   int Flags = OptFlags(L, 4, 0);
-  StartPos < 0 ? StartPos = 0 : StartPos > (intptr_t)size ? StartPos = size : 0;
-  EndPos < StartPos ? EndPos = StartPos : EndPos > (intptr_t)size ? EndPos = size : 0;
-  str = GetPluginStartupInfo(L)->FSF->XLat(Line, StartPos, EndPos, Flags);
-  str ? push_utf8_string(L, str, -1) : lua_pushnil(L);
+  Line = GetFSF(L)->XLat(Line, StartPos, EndPos, Flags);
+  Line ? push_utf8_string(L, Line, -1) : lua_pushnil(L);
   return 1;
 }
 

commit e00e74d6ac30371a0a6436d8fb0560228f68c8d8

Fix a serious bug introduced in ea09826644a588518b282ca4944fd5421fafbeba

far2l/src/console/keyboard.cpp
@@ -1795,8 +1795,8 @@ int TranslateKeyToVK(int Key,int &VirtKey,int &ControlState,INPUT_RECORD *Rec)
 			VirtKey=FKey;
 		else if ((unsigned int)FKey > KEY_FKEY_BEGIN && (unsigned int)FKey < KEY_END_FKEY)
 			VirtKey=FKey-KEY_FKEY_BEGIN;
-		//else if (FKey < MAX_VKEY_CODE)
-		//	VirtKey=WINPORT(VkKeyScan)(FKey);
+		else if (FKey < MAX_VKEY_CODE)
+			VirtKey=WINPORT(VkKeyScan)(FKey);
 		else
 			VirtKey=FKey;
 	}