1d2c57a17203ff03e805a6b805592a13c38bdcc7
Shmuel Zeigerman <solomuz0@gmail.com>
2023-03-30 21:45:40 +0300

    Add win.GetLocalTime function

--- luafar/src/service.c
+++ luafar/src/service.c
@@ -4579,6 +4579,14 @@ int win_GetSystemTime(lua_State *L)
   return 1;
 }
 
+int win_GetLocalTime(lua_State *L)
+{
+  SYSTEMTIME st;
+  WINPORT(GetLocalTime)(&st);
+  pushSystemTime(L, &st);
+  return 1;
+}
+
 int win_CompareString (lua_State *L)
 {
   size_t len1, len2;
@@ -5838,6 +5846,7 @@ static const luaL_Reg win_funcs[] = {
   {"SystemTimeToFileTime",       win_SystemTimeToFileTime},
   {"GetSystemTimeAsFileTime",    win_GetSystemTimeAsFileTime},
   {"GetSystemTime",              win_GetSystemTime},
+  {"GetLocalTime",               win_GetLocalTime},
   {"CompareString",              win_CompareString},
   {"wcscmp",                     win_wcscmp},
   {"ExtractKey",                 win_ExtractKey},
