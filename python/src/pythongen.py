f9ca86ab23f223a97f944de1c0bc3e07af6d0929
Shmuel Zeigerman <solomuz0@gmail.com>
2023-04-26 13:48:11 +0300

    Fix Python plugin build

--- python/src/pythongen.py
+++ python/src/pythongen.py
@@ -11,7 +11,7 @@ target = os.path.join(sys.argv[2], "far2lcffi.py")
 
 cpp = pcpp.Preprocessor()
 cpp.add_path(source)
-cpp.add_path(os.path.join(source, "far2l", "far2sdk"))
+cpp.add_path(os.path.join(source, "far", "far2sdk"))
 cpp.add_path(sys.argv[2])  # to find consts.h
 cpp.define("FAR_USE_INTERNALS 1")
 # cpp.define('WINPORT_DIRECT')
