3f1defff076545daf60cf894c0a2b5d3e1ecbe65
Shmuel Zeigerman <solomuz0@gmail.com>
2023-07-06 22:18:50 +0300

    Extend minilua to handle -e option

--- luafar/src/minilua.c
+++ luafar/src/minilua.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <string.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
@@ -10,17 +10,41 @@ int main(int argc, char **argv)
 
   if (argc >= 2 && (L = lua_open()))
   {
+    int i, j=-1, execute=0;
+
     luaL_openlibs(L);
-    status = luaL_loadfile(L, argv[1]);
-    if (status == 0)
+
+    for (i=1,status=0; i<argc && status==0; i++)
     {
-      int i;
-      for (i=2; i<argc; i++)
+      if (j < 0)
+      {
+        if (execute)
+        {
+          status = luaL_loadstring(L, argv[i]) || lua_pcall(L,0,0,0);
+          execute = 0;
+        }
+        else if (!strncmp(argv[i], "-e", 2))
+        {
+          if (argv[i][2])
+            status = luaL_loadstring(L, argv[i]+2) || lua_pcall(L,0,0,0);
+          else
+            execute = 1;
+        }
+        else
+        {
+          status = luaL_loadfile(L, argv[i]);
+          j = 0;
+        }
+      }
+      else
       {
         lua_pushstring(L, argv[i]);
+        j++;
       }
-      status = lua_pcall(L, argc-2, 0, 0);
     }
+
+    if (status == 0 && j >= 0)
+      status = lua_pcall(L, j, 0, 0);
     lua_close(L);
   }
 
