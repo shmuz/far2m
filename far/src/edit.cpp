d91b494d323f108948119dab76dbd488b16a4da7
Shmuel Zeigerman <solomuz0@gmail.com>
2023-09-02 10:31:52 +0300

    Editor: fix sporadic crash on clipboard paste (from far2l)

--- far/src/edit.cpp
+++ far/src/edit.cpp
@@ -1602,7 +1602,7 @@ int Edit::InsertKey(int Key)
 					return FALSE;
 
 				Str=NewStr;
-				wmemset(&Str[StrSize], L' ', CurPos - StrSize);
+				wmemset(&Str[StrSize], L' ', CurPos + 1 - StrSize);
 				Str[CurPos + 1] = 0;
 				StrSize=CurPos+1;
 			}
@@ -1980,7 +1980,7 @@ void Edit::InsertBinaryString(const wchar_t *Str,int Length)
 
 				this->Str=NewStr;
 				wmemset(&this->Str[StrSize], L' ', CurPos - StrSize);
-				this->Str[CurPos + 1] = 0;
+				this->Str[CurPos] = 0;
 				StrSize=CurPos;
 			}
 
