c550d8f831c6fd70040c2ca370e85b58279cb075
Shmuel Zeigerman <solomuz0@gmail.com>
2023-09-09 21:17:14 +0300

    Refactoring

--- netcfg/src/GlobalInfo.cpp
+++ netcfg/src/GlobalInfo.cpp
@@ -3,7 +3,7 @@
 #ifdef FAR_MANAGER_FAR2M
 SHAREDSYMBOL void WINAPI EXP_NAME(GetGlobalInfo)(struct GlobalInfo *aInfo)
 {
-  struct VersionInfo Version = { 0,0,0,0 };
+  struct VersionInfo Version = { 2023,9,1,0 };
   aInfo->StructSize    = sizeof(*aInfo);
   aInfo->SysID         = 0xE873426D;
   aInfo->Version       = Version;
