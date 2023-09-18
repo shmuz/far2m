c187c4d7c1cfb6da15af72e9dd0f9033584d7546
Shmuel Zeigerman <solomuz0@gmail.com>
2023-09-18 07:34:41 +0300

    Restore compatibility with old LuaJIT builds (fixes #42)

--- luafar/src/makeflags.lua
+++ luafar/src/makeflags.lua
@@ -1,6 +1,6 @@
 -- This script is intended to generate the "flags.cpp" file
 
-local function add_defines (src, trg_int, trg_ptr)
+local function add_defines (src, trg_int)
   local skip = false
   for line in src:gmatch("[^\r\n]+") do
     if line:find("^%s*//") then
@@ -12,8 +12,7 @@ local function add_defines (src, trg_int, trg_ptr)
     else
       local c, v = line:match("#define%s+([A-Z][A-Z0-9_]*)%s+(.+)")
       if c then
-        local trg = (v:find("%(HANDLE%)") or v:find("%(void%*%)")) and trg_ptr or trg_int
-        table.insert(trg, c)
+        table.insert(trg_int, c)
       end
     end
   end
@@ -57,20 +56,9 @@ static const flag_pair flags[] = {
 ]]
   table.sort(trg_int)
   for k,v in ipairs(trg_int) do
-    local len = math.max(1, 32 - #v)
+    local len = math.max(1, 40 - #v)
     local space = (" "):rep(len)
-    io.write(string.format('  {"%s",%s%s },\n', v, space, v))
-  end
-  io.write("};\n\n")
-
-  io.write [[
-static const ptr_pair pflags[] = {
-]]
-  table.sort(trg_ptr)
-  for k,v in ipairs(trg_ptr) do
-    local len = math.max(1, 32 - #v)
-    local space = (" "):rep(len)
-    io.write(string.format('  {"%s",%s%s },\n', v, space, v))
+    io.write(string.format('  {"%s",%s(int64_t) %s },\n', v, space, v))
   end
   io.write("};\n\n")
 end
@@ -117,11 +105,6 @@ typedef struct {
   int64_t val;
 } flag_pair;
 
-typedef struct {
-  const char* key;
-  void* val;
-} ptr_pair;
-
 ]]
 
 
@@ -134,11 +117,6 @@ void add_flags (lua_State *L)
     lua_pushnumber(L, flags[i].val);
     lua_setfield(L, -2, flags[i].key);
   }
-  nelem = sizeof(pflags) / sizeof(pflags[0]);
-  for (i=0; i<nelem; ++i) {
-    lua_pushlightuserdata(L, pflags[i].val);
-    lua_setfield(L, -2, pflags[i].key);
-  }
 }
 
 ]]
@@ -147,7 +125,7 @@ do
   local dir = ...
   assert (dir, "input directory not specified")
 
-  local trg_int, trg_ptr = {}, {}
+  local trg_int = {}
   for _,v in ipairs(t_winapi) do table.insert(trg_int, v) end
 
   for _,fname in ipairs { "farplug-wide.h", "farcolor.h", "farkeys.h" } do
@@ -155,14 +133,14 @@ do
     local src = fp:read ("*all")
     fp:close()
     if fname == "farplug-wide.h" then
-      add_defines(src, trg_int, trg_ptr)
+      add_defines(src, trg_int)
       add_static(src, trg_int)
     end
     add_enums(src, trg_int)
   end
 
   io.write(file_top)
-  write_target(trg_int, trg_ptr)
+  write_target(trg_int)
   io.write(file_bottom)
 end
 
