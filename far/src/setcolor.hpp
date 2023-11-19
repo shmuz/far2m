1df014d8268c3bff9560a329e0bc6fb8e73bf026
Shmuel Zeigerman <solomuz0@gmail.com>
2023-11-19 20:54:10 +0200

    Update setcolor.cpp/.hpp from far2l

--- far/src/setcolor.hpp
+++ far/src/setcolor.hpp
@@ -36,4 +36,5 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <WinCompat.h>
 
 void SetColors();
-int GetColorDialog(WORD& Color,bool bCentered=false,bool bAddTransparent=false,INT_PTR PluginNumber=-1);
+bool GetColorDialogForFileFilter(DWORD64 &Color, INT_PTR PluginNumber=-1);
+bool GetColorDialog(WORD &Color, bool bCentered=false, bool bAddTransparent=false, INT_PTR PluginNumber=-1);
