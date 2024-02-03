53ab3e9db08049315e0a27b8e33a6355e135a7b8
Shmuel Zeigerman <solomuz0@gmail.com>
2024-02-03 02:17:36 +0200

    Remove timeout logic from SetFarTitle (touch #52)

--- far/src/console/constitle.cpp
+++ far/src/console/constitle.cpp
@@ -122,7 +122,7 @@ void ConsoleTitle::SetFarTitle(const wchar_t *Title, bool Force, bool Restoring)
 			!(CtrlObject->Macro.IsExecuting() && CtrlObject->Macro.IsOutputDisabled()))
 		{
 			DWORD CurTime=WINPORT(GetTickCount)();
-			if(CurTime-ShowTime > RedrawTimeout || Force)
+			// if(CurTime-ShowTime > RedrawTimeout || Force)
 			{
 				ShowTime=CurTime;
 				Console.SetTitle(strFarTitle);
