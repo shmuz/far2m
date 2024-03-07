7d20b7344ca58efc18f4dce5b5964e49fe8f25f5
Shmuel Zeigerman <solomuz0@gmail.com>
2024-03-07 11:28:23 +0200

    LuaMacro: auto-generate constants.lua on plugin build stage

--- luamacro/src/GlobalInfo.c
+++ luamacro/src/GlobalInfo.c
@@ -2,7 +2,7 @@
 
 SHAREDSYMBOL void WINAPI GetGlobalInfoW(struct GlobalInfo *aInfo)
 {
-  struct VersionInfo Version = { 3,0,0,800 };
+  struct VersionInfo Version = { 3,0,0,801 };
   aInfo->StructSize    = sizeof(*aInfo);
   aInfo->SysID         = 0x4EBBEFC8;
   aInfo->Version       = Version;
