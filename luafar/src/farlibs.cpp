a6c38e935246d36744fa02d26ea67a924b15a30d
Shmuel Zeigerman <solomuz0@gmail.com>
2024-04-07 15:46:39 +0300

    LuaFAR: remove far.SudoClientRegion, leave SuduCRCall

--- luafar/src/farlibs.cpp
+++ luafar/src/farlibs.cpp
@@ -1,8 +1,6 @@
 #include <utils.h>
 #include <sudo.h>
 
-const char SudoClientRegionType[] = "SudoClientRegion";
-
 extern "C"
 {
 #include <lua.h>
@@ -40,54 +38,6 @@ int far_GetMyHome(lua_State *L)
 {
 	const std::string &ret = GetMyHome();
 	lua_pushstring(L, ret.c_str());
-	return 1;
-}
-
-static SudoClientRegion ** CheckSudoClientRegion(lua_State* L, int pos)
-{
-	return (SudoClientRegion **)luaL_checkudata(L, pos, SudoClientRegionType);
-}
-
-static int SudoClientRegion_Free(lua_State *L)
-{
-	SudoClientRegion **scr = CheckSudoClientRegion(L, 1);
-	if (*scr) {
-		delete(*scr);
-		*scr = NULL;
-	}
-	return 0;
-}
-
-static int SudoClientRegion_tostring (lua_State *L)
-{
-	SudoClientRegion **scr = CheckSudoClientRegion(L, 1);
-	if (*scr)
-		lua_pushfstring(L, "%s (%p)", SudoClientRegionType, *scr);
-	else
-		lua_pushfstring(L, "%s (closed)", SudoClientRegionType);
-	return 1;
-}
-
-static const luaL_Reg SudoClientRegion_methods[] = {
-	{ "Close",        SudoClientRegion_Free },
-	{ "__gc",         SudoClientRegion_Free },
-	{ "__tostring",   SudoClientRegion_tostring },
-	{ NULL, NULL },
-};
-
-int far_SudoClientRegion(lua_State *L)
-{
-	SudoClientRegion **scr = (SudoClientRegion**) lua_newuserdata(L, sizeof(SudoClientRegion*));
-	*scr = new SudoClientRegion();
-	luaL_getmetatable(L, SudoClientRegionType);
-	if (lua_isnil(L, -1)) {
-		lua_pop(L, 1);
-		luaL_newmetatable(L, SudoClientRegionType);
-		lua_pushvalue(L, -1);
-		lua_setfield(L, -2, "__index");
-		luaL_register(L, NULL, SudoClientRegion_methods);
-	}
-	lua_setmetatable(L, -2);
 	return 1;
 }
 
