f01565adf1cc390f454de7e1f711b8ef2c4e96bd
Shmuel Zeigerman <solomuz0@gmail.com>
2024-04-29 14:36:52 +0300

    Update plugin Colorer from far2l

--- colorer/src/pcolorer2/GlobalInfo.cpp
+++ colorer/src/pcolorer2/GlobalInfo.cpp
@@ -2,11 +2,11 @@
 
 SHAREDSYMBOL void WINAPI EXP_NAME(GetGlobalInfo)(struct GlobalInfo *aInfo)
 {
-  struct VersionInfo Version = { 0,0,0,0 };
+  struct VersionInfo Version = { 1,4,0,0 };
   aInfo->StructSize    = sizeof(*aInfo);
   aInfo->SysID         = 0xD2F36B62;
   aInfo->Version       = Version;
   aInfo->Title         = L"FarColorer";
   aInfo->Description   = L"Syntax highlighting in Far editor";
-  aInfo->Author        = L"Igor Ruskih, FAR People";
+  aInfo->Author        = L"Igor Ruskih, Dobrunov Aleksey, FAR People";
 }
