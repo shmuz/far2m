f284c3e0f249d0309cdf479cbc84672c44aa725b
Shmuel Zeigerman <solomuz0@gmail.com>
2024-05-20 10:37:59 +0300

    Reformat ctrlobj.cpp

--- far/src/ctrlobj.cpp
+++ far/src/ctrlobj.cpp
@@ -33,7 +33,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "headers.hpp"
 
-
 #include "ctrlobj.hpp"
 #include "lang.hpp"
 #include "language.hpp"
@@ -56,7 +55,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ControlObject *CtrlObject;
 
-ControlObject::ControlObject():
+ControlObject::ControlObject()
+	:
 	FPanels(0),
 	CmdLine(0),
 	CmdHistory(0),
@@ -69,40 +69,41 @@ ControlObject::ControlObject():
 	EditorPosCache(0)
 {
 	_OT(SysLog(L"[%p] ControlObject::ControlObject()", this));
-	CtrlObject=this;
+	CtrlObject = this;
 	HiFiles = new HighlightFiles;
 	ViewerPosCache = new FilePositionCache(FPCK_VIEWER);
 	EditorPosCache = new FilePositionCache(FPCK_EDITOR);
 	FrameManager = new Manager;
 	ApplyConfig();
-	CmdHistory=new History(HISTORYTYPE_CMD,Opt.HistoryCount,"SavedHistory",&Opt.SaveHistory,false);
-	FolderHistory=new History(HISTORYTYPE_FOLDER,Opt.FoldersHistoryCount,"SavedFolderHistory",&Opt.SaveFoldersHistory,true);
-	ViewHistory=new History(HISTORYTYPE_VIEW,Opt.ViewHistoryCount,"SavedViewHistory",&Opt.SaveViewHistory,true);
-	FolderHistory->SetAddMode(true,2,true);
+	CmdHistory = new History(HISTORYTYPE_CMD, Opt.HistoryCount, "SavedHistory", &Opt.SaveHistory, false);
+	FolderHistory = new History(HISTORYTYPE_FOLDER, Opt.FoldersHistoryCount, "SavedFolderHistory",
+			&Opt.SaveFoldersHistory, true);
+	ViewHistory = new History(HISTORYTYPE_VIEW, Opt.ViewHistoryCount, "SavedViewHistory",
+			&Opt.SaveViewHistory, true);
+	FolderHistory->SetAddMode(true, 2, true);
 	ViewHistory->SetAddMode(true, 1, true);
 }
-
 
 void ControlObject::Init()
 {
 	TreeList::ClearCache(0);
 	SetColor(COL_COMMANDLINEUSERSCREEN);
-	GotoXY(0,ScrY-3);
+	GotoXY(0, ScrY - 3);
 	ShowStartupBanner();
-	GotoXY(0,ScrY-2);
-	MoveCursor(0,ScrY-1);
-	FPanels=new FilePanels();
-	CmdLine=new CommandLine();
-	CmdLine->SaveBackground(0,0,ScrX,ScrY);
-	this->MainKeyBar=&(FPanels->MainKeyBar);
-	this->TopMenuBar=&(FPanels->TopMenuBar);
+	GotoXY(0, ScrY - 2);
+	MoveCursor(0, ScrY - 1);
+	FPanels = new FilePanels();
+	CmdLine = new CommandLine();
+	CmdLine->SaveBackground(0, 0, ScrX, ScrY);
+	this->MainKeyBar = &(FPanels->MainKeyBar);
+	this->TopMenuBar = &(FPanels->TopMenuBar);
 	FPanels->Init();
 	FPanels->SetScreenPosition();
 
 	if (Opt.ShowMenuBar)
 		this->TopMenuBar->Show();
 
-//  FPanels->Redraw();
+	//  FPanels->Redraw();
 	CmdLine->Show();
 
 	this->MainKeyBar->Refresh(Opt.ShowKeyBar);
@@ -131,13 +132,13 @@ void ControlObject::Init()
 	/*
 		FarChDir(StartCurDir);
 	*/
-//  _SVS(SysLog(L"ActivePanel->GetCurDir='%ls'",StartCurDir));
-//  _SVS(char PPP[NM];Cp()->GetAnotherPanel(Cp()->ActivePanel)->GetCurDir(PPP);SysLog(L"AnotherPanel->GetCurDir='%ls'",PPP));
+	//  _SVS(SysLog(L"ActivePanel->GetCurDir='%ls'",StartCurDir));
+	//  _SVS(char PPP[NM];Cp()->GetAnotherPanel(Cp()->ActivePanel)->GetCurDir(PPP);SysLog(L"AnotherPanel->GetCurDir='%ls'",PPP));
 }
 
 void ControlObject::CreateFilePanels()
 {
-	FPanels=new FilePanels();
+	FPanels = new FilePanels();
 }
 
 ControlObject::~ControlObject()
@@ -147,13 +148,11 @@ ControlObject::~ControlObject()
 
 	_OT(SysLog(L"[%p] ControlObject::~ControlObject()", this));
 
-	if (Cp()&&Cp()->ActivePanel)
-	{
+	if (Cp() && Cp()->ActivePanel) {
 		if (Opt.AutoSaveSetup)
 			ConfigOptSave(false);
 
-		if (Cp()->ActivePanel->GetMode()!=PLUGIN_PANEL)
-		{
+		if (Cp()->ActivePanel->GetMode() != PLUGIN_PANEL) {
 			FARString strCurDir;
 			Cp()->ActivePanel->GetCurDir(strCurDir);
 			FolderHistory->AddToHistory(strCurDir);
@@ -161,7 +160,7 @@ ControlObject::~ControlObject()
 	}
 
 	FrameManager->CloseAll();
-	FPanels=nullptr;
+	FPanels = nullptr;
 	FileFilter::CloseFilter();
 	delete CmdHistory;
 	delete FolderHistory;
@@ -175,19 +174,18 @@ ControlObject::~ControlObject()
 	delete FrameManager;
 	TreeList::FlushCache();
 	Lang.Close();
-	CtrlObject=nullptr;
+	CtrlObject = nullptr;
 }
-
 
 void ControlObject::ShowStartupBanner(LPCWSTR EmergencyMsg)
 {
 	std::vector<FARString> Lines;
 
-	for (const char *p = Copyright; *p; ) {
+	for (const char *p = Copyright; *p;) {
 		const char *q = strchr(p, '\n');
 		if (q) {
-			Lines.emplace_back(std::string(p, q-p));
-			p = q+1;
+			Lines.emplace_back(std::string(p, q - p));
+			p = q + 1;
 		} else {
 			Lines.emplace_back(std::string(p));
 			break;
@@ -207,7 +205,7 @@ void ControlObject::ShowStartupBanner(LPCWSTR EmergencyMsg)
 			Console.SetCursorPosition(CursorPosition);
 		}
 
-		Console.SetTextAttributes(F_YELLOW|B_BLACK);
+		Console.SetTextAttributes(F_YELLOW | B_BLACK);
 		Console.Write(EmergencyMsg, wcslen(EmergencyMsg));
 		CursorPosition.Y++;
 		Console.SetCursorPosition(CursorPosition);
@@ -234,13 +232,13 @@ void ControlObject::ShowStartupBanner(LPCWSTR EmergencyMsg)
 		const int FreeSpace = Size.Y - CursorPosition.Y - 1;
 		const int LineCount = 4 + Lines.size();
 
-		if (FreeSpace<LineCount)
-			ScrollScreen(LineCount-FreeSpace);
+		if (FreeSpace < LineCount)
+			ScrollScreen(LineCount - FreeSpace);
 
 		const auto SavedColor = GetColor();
 		for (size_t i = 0; i < Lines.size(); ++i) {
 			if (i >= ConsoleHintsIndex) {
-				SetColor(Lines[i].Begins(L' ') ? COL_HELPTEXT : COL_HELPTOPIC);//COL_HELPBOXTITLE
+				SetColor(Lines[i].Begins(L' ') ? COL_HELPTEXT : COL_HELPTOPIC);    // COL_HELPBOXTITLE
 			}
 			if (!Lines[i].IsEmpty()) {
 				GotoXY(0, ScrY - (Lines.size() - i + 2));
@@ -249,10 +247,9 @@ void ControlObject::ShowStartupBanner(LPCWSTR EmergencyMsg)
 		}
 		SetRealColor(SavedColor);
 	}
-
 }
 
-FilePanels* ControlObject::Cp()
+FilePanels *ControlObject::Cp()
 {
 	return FPanels;
 }
