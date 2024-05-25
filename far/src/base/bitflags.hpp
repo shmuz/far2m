48103813a3df585125cf3de992d0ad25faab132a
Shmuel Zeigerman <solomuz0@gmail.com>
2024-05-25 22:28:54 +0300

    Reformat and update from far2l some src/base files

--- far/src/base/bitflags.hpp
+++ far/src/base/bitflags.hpp
@@ -37,26 +37,48 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class BitFlags
 {
-	public:
-		DWORD Flags;
+public:
+	DWORD Flags;
 
-	public:
-		BitFlags() {Flags=0;}
-		BitFlags(DWORD Fl) {Flags=Fl;}
+public:
+	BitFlags() { Flags = 0; }
+	BitFlags(DWORD Fl) { Flags = Fl; }
 
-		~BitFlags() {}
+	~BitFlags() {}
 
-	public:
-		// установить набор флагов
-		DWORD Set(DWORD NewFlags) { Flags|=NewFlags; return Flags;}
-		// сбросить набор флагов
-		DWORD Clear(DWORD NewFlags) { Flags&=~NewFlags; return Flags; }
-		// проверить набор флагов
-		bool Check(DWORD NewFlags) const { return (Flags & NewFlags) != 0; }
-		// изменить состояние набора флагов в заивисмости от Status
-		DWORD Change(DWORD NewFlags,BOOL Status) { if (Status) Flags|=NewFlags; else Flags&=~NewFlags; return Flags;}
-		// инвертировать состояние флагов
-		DWORD Swap(DWORD SwapedFlags) { if (Flags&SwapedFlags) Flags&=~SwapedFlags; else Flags|=SwapedFlags; return Flags;}
-		//сбросить все флаги
-		void ClearAll() {Flags=0;}
+public:
+	// установить набор флагов
+	DWORD Set(DWORD NewFlags)
+	{
+		Flags|= NewFlags;
+		return Flags;
+	}
+	// сбросить набор флагов
+	DWORD Clear(DWORD NewFlags)
+	{
+		Flags&= ~NewFlags;
+		return Flags;
+	}
+	// проверить набор флагов
+	bool Check(DWORD NewFlags) const { return (Flags & NewFlags) != 0; }
+	// изменить состояние набора флагов в заивисмости от Status
+	DWORD Change(DWORD NewFlags, BOOL Status)
+	{
+		if (Status)
+			Flags|= NewFlags;
+		else
+			Flags&= ~NewFlags;
+		return Flags;
+	}
+	// инвертировать состояние флагов
+	DWORD Swap(DWORD SwappedFlags)
+	{
+		if (Flags & SwappedFlags)
+			Flags&= ~SwappedFlags;
+		else
+			Flags|= SwappedFlags;
+		return Flags;
+	}
+	// сбросить все флаги
+	void ClearAll() { Flags = 0; }
 };
