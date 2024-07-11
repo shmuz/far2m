shmuz/far2m

commit cd4a44d74b61881b361fb9d8ef5676d575903035

Support menu items GUID's identifiers (only for LuaMacro; touch #61)

far/src/plug/plugins.cpp
@@ -94,6 +94,17 @@ static const char *HotKeyType(int Type)
 	}
 }
 
+static const GUID *MenuItemGuids(int Type, const PluginInfo *Info)
+{
+	switch(Type)
+	{
+		default:
+		case MTYPE_COMMANDSMENU: return Info->PluginMenuGuids;
+		case MTYPE_CONFIGSMENU:  return Info->PluginConfigGuids;
+		case MTYPE_DISKSMENU:    return Info->DiskMenuGuids;
+	}
+}
+
 const char *PluginsIni()
 {
 	static std::string s_out(InMyConfig("plugins/state.ini"));
@@ -145,6 +156,7 @@ PluginManager::PluginManager():
 	PluginsData(nullptr),
 	PluginsCount(0),
 	OemPluginsCount(0),
+	ptrLMInfo(nullptr),
 	CurEditor(nullptr),
 	CurViewer(nullptr)
 {
@@ -1251,6 +1263,8 @@ void PluginManager::Configure(int StartPos)
 	if (Opt.Policies.DisabledOptions&FFPOL_MAINMENUPLUGINS)
 		return;
 
+	UpdateLMInfo(); // do this before macro area changes
+
 	{
 		VMenu PluginList(Msg::PluginConfigTitle,nullptr,0,ScrY-4);
 		PluginList.SetFlags(VMENU_WRAPMODE);
@@ -1412,6 +1426,8 @@ int PluginManager::CommandsMenu(int ModalType,int StartPos,const wchar_t *Histor
 			return 0;
 		}
 	}
+
+	UpdateLMInfo(); // do this before macro area changes
 
 	int MenuItemNumber=0;
 	int Editor = ModalType==MODALTYPE_EDITOR,
@@ -1655,6 +1671,21 @@ int PluginManager::CommandsMenu(int ModalType,int StartPos,const wchar_t *Histor
 
 std::string PluginManager::GetHotKeySettingName(Plugin *pPlugin, int ItemNumber, MENUTYPE MenuType)
 {
+	if (pPlugin->SysID == SYSID_LUAMACRO && ptrLMInfo)
+	{
+		const GUID *Guid = MenuItemGuids(MenuType, ptrLMInfo);
+		if (Guid) {
+			auto Array = reinterpret_cast<const unsigned char*>(Guid + ItemNumber);
+			char Buf[2 * sizeof(GUID) + 1];
+			char *Trg = Buf;
+
+			for (size_t I=0; I < sizeof(GUID); I++) {
+				Trg += sprintf(Trg, "%02X", (unsigned int)Array[I]);
+			}
+			std::string out = StrPrintf("luamacro:%s#%s", HotKeyType(MenuType), Buf);
+			return out;
+		}
+	}
 	std::string out = pPlugin->GetSettingsName();
 	out+= StrPrintf(":%s#%d", HotKeyType(MenuType), ItemNumber);
 	return out;
@@ -1662,12 +1693,12 @@ std::string PluginManager::GetHotKeySettingName(Plugin *pPlugin, int ItemNumber,
 
 void PluginManager::GetPluginHotKey(Plugin *pPlugin, int ItemNumber, MENUTYPE MenuType, FARString &strHotKey)
 {
-	strHotKey = KeyFileReadSection(PluginsIni(), SettingsSection).GetString(
-		GetHotKeySettingName(pPlugin, ItemNumber, MenuType));
+	KeyFileReadSection kfh(PluginsIni(), SettingsSection);
+	strHotKey = kfh.GetString(GetHotKeySettingName(pPlugin, ItemNumber, MenuType));
 }
 
 bool PluginManager::SetHotKeyDialog(
-		const wchar_t *DlgPluginTitle,		// имя плагина
+		const wchar_t *DlgPluginTitle,    // имя плагина
 		Plugin *pPlugin,                  // ключ, откуда берем значение в state.ini/Settings
 		int ItemNumber,                   // +
 		MENUTYPE MenuType)                // +
@@ -2552,3 +2583,13 @@ void PluginManager::ShowPluginInfo(Plugin* pPlugin)
 	Builder.AddOK();
 	Builder.ShowDialog();
 }
+
+void PluginManager::UpdateLMInfo()
+{
+	ptrLMInfo = nullptr;
+	if (auto pPlugin = FindPlugin(SYSID_LUAMACRO)) {
+		if (pPlugin->GetPluginInfo(&LMInfo))
+			ptrLMInfo = &LMInfo;
+	}
+}
+

far/src/plug/plugins.hpp
@@ -191,6 +191,8 @@ class PluginManager
 		int OemPluginsCount;
 		struct BackgroundTasks : std::map<std::wstring, unsigned int>, std::mutex {} BgTasks;
 		std::unordered_map<DWORD, Plugin*> SysIdMap;
+		PluginInfo LMInfo; // luamacro
+		PluginInfo *ptrLMInfo;
 
 	public:
 
@@ -282,6 +284,8 @@ class PluginManager
 		// $ .09.2000 SVS - Функция CallPlugin - найти плагин по ID и запустить OpenFrom = OPEN_*
 		int CallPlugin(DWORD SysID,int OpenFrom, void *Data, void **Ret=nullptr);
 		bool CallPluginItem(DWORD SysID, CallPluginInfo* Data);
+
+		void UpdateLMInfo(); //luamacro
 
 //api functions
 

far/src/panels/panel.cpp
@@ -382,6 +382,8 @@ static void AddBookmarkItems(VMenu &ChDisk, int Pos)
 
 int Panel::ChangeDiskMenu(int Pos,int FirstCall)
 {
+	CtrlObject->Plugins.UpdateLMInfo(); // do this before macro area changes
+
 	/*Events.DeviceArivalEvent.Reset();
 	Events.DeviceRemoveEvent.Reset();
 	Events.MediaArivalEvent.Reset();

luafar/src/exported.c
@@ -1114,12 +1114,14 @@ static void SetMenuGuids(lua_State *L, const char *Field, size_t Count, const GU
 {
 	if (Count) {
 		lua_getfield(L, -1, Field);
-		if (lua_type(L,-1) == LUA_TSTRING && lua_objlen(L,-1) >= Count*sizeof(GUID)) {
-			*Target = (const GUID*)lua_tostring(L,-1);
-			lua_rawseti(L, CPos, lua_objlen(L, CPos) + 1);
+		if (lua_type(L,-1) == LUA_TSTRING) {
+			if (lua_objlen(L,-1) >= Count*sizeof(GUID)) {
+				*Target = (const GUID*)lua_tostring(L,-1);
+				lua_rawseti(L, CPos, lua_objlen(L, CPos) + 1);
+				return;
+			}
 		}
-		else Log(L,"guids problem"),
-			lua_pop(L,1);
+		lua_pop(L, 1);
 	}
 }
 

luamacro/src/farapi.lua
@@ -2161,6 +2161,9 @@ struct PluginInfo
 	int PluginConfigStringsNumber;
 	const wchar_t *CommandPrefix;
 	DWORD SysID;
+	const GUID *DiskMenuGuids;
+	const GUID *PluginMenuGuids;
+	const GUID *PluginConfigGuids;
 };
 
 

luamacro/src/luamacro.lua
@@ -108,6 +108,8 @@ end
 -- END: Functions implemented via "returning a key" to Far
 -------------------------------------------------------------------------------
 
+local PluginInfo
+
 function export.GetPluginInfo()
   local out = {
     Flags = bor(F.PF_PRELOAD,F.PF_FULLCMDLINE,F.PF_EDITOR,F.PF_VIEWER,F.PF_DIALOG),
@@ -115,6 +117,7 @@ function export.GetPluginInfo()
     PluginMenuGuids = win.Uuid("EF6D67A2-59F7-4DF3-952E-F9049877B492"),
     PluginMenuStrings = { "Macro Browser" },
   }
+  PluginInfo = out
 
   local mode = far.MacroGetArea()
   local area = utils.GetTrueAreaName(mode)

commit 8f28d6272b3fffdbd73bb670d2b491cf72ce61d5

Fix Plugin.Menu and Plugin.Config broken since cd4a44d7

far/src/macro/macro.cpp
@@ -86,6 +86,43 @@ int Log(const char* Format, ...)
 	return N;
 }
 
+static bool StrToGuid(const wchar_t *Str, GUID& Guid)
+{
+	const char tmpl[] = "HHHHHHHH-HHHH-HHHH-HHHH-HHHHHHHHHHHH";
+
+	for (int i=0; ; i++) {
+		if (tmpl[i] == 'H') {
+			if (!iswxdigit(Str[i]))
+				return false;
+		}
+		else if (tmpl[i] != Str[i])
+			return false;
+
+		if (!tmpl[i])
+			break;
+	}
+
+	uint32_t val;
+	uint8_t buf[20];
+	wchar_t tmp[] = {0,0,0};
+
+	for (int i=0,j=0; tmpl[i]; ) {
+		if (Str[i] == L'-') {
+			i++;
+		}
+		tmp[0] = Str[i++];
+		tmp[1] = Str[i++];
+		swscanf(tmp, L"%X", &val);
+		buf[j++] = val;
+	}
+
+	Guid.Data1 = (buf[0]<<24) + (buf[1]<<16) + (buf[2]<<8) + buf[3];
+	Guid.Data2 = (buf[4]<<8) + buf[5];
+	Guid.Data3 = (buf[6]<<8) + buf[7];
+	memcpy(Guid.Data4, buf+8, 8);
+	return true;
+}
+
 // для диалога назначения клавиши
 struct DlgParam
 {
@@ -581,11 +618,24 @@ FarKey KeyMacro::GetKey()
 				if (!CtrlObject->Plugins.FindPlugin(SysID))
 					break;
 
+				bool IsLuamacro = (SysID == SYSID_LUAMACRO);
+				GUID Guid;
+
 				PluginManager::CallPluginInfo cpInfo = { CPT_CHECKONLY };
 				if (mpr.ReturnType == MPRT_PLUGINMENU || mpr.ReturnType == MPRT_PLUGINCONFIG)
 				{
-					cpInfo.ItemUuid = mpr.Count > 1 && mpr.Values[1].Type == FMVT_DOUBLE ?
-						static_cast<DWORD>(mpr.Values[1].Double) : 0;
+					if (!IsLuamacro) {
+						cpInfo.ItemNumber = mpr.Count > 1 && mpr.Values[1].Type == FMVT_DOUBLE ?
+							static_cast<DWORD>(mpr.Values[1].Double) : 0;
+					}
+					else {
+						if (mpr.Count > 1 && mpr.Values[1].Type == FMVT_STRING) {
+							if (StrToGuid(mpr.Values[1].String, Guid))
+								cpInfo.ItemUuid = &Guid;
+							else
+								break;
+						}
+					}
 				}
 
 				if (mpr.ReturnType == MPRT_PLUGINMENU)

far/src/plug/PluginA.hpp
@@ -142,6 +142,7 @@ class PluginA: public Plugin
 		bool HasProcessHostFile() { return pProcessHostFile!=nullptr; }
 		bool HasSetFindList() { return pSetFindList!=nullptr; }
 		bool HasConfigure() { return pConfigure!=nullptr; }
+		bool HasConfigureV3() { return false; }
 		bool HasMayExitFAR() { return pMayExitFAR!=nullptr; }
 		bool HasExitFAR() { return pExitFAR!=nullptr; }
 		bool HasProcessKey() { return pProcessKey!=nullptr; }

far/src/plug/plclass.hpp
@@ -123,6 +123,7 @@ class Plugin
 		virtual bool HasProcessHostFile() = 0;
 		virtual bool HasSetFindList() = 0;
 		virtual bool HasConfigure() = 0;
+		virtual bool HasConfigureV3() = 0;
 		virtual bool HasMayExitFAR() = 0;
 		virtual bool HasExitFAR() = 0;
 		virtual bool HasProcessKey() = 0;

far/src/plug/plugins.cpp
@@ -2107,6 +2107,7 @@ int PluginManager::CallPlugin(DWORD SysID, int OpenFrom, void *Data, void **Ret)
 // поддержка макрофункций plugin.call, plugin.cmd, plugin.config и т.п
 bool PluginManager::CallPluginItem(DWORD SysID, CallPluginInfo *Data)
 {
+	auto IsLuamacro = (SysID == SYSID_LUAMACRO);
 	auto Result = false;
 
 	Frame *TopFrame = FrameManager->GetTopModal();
@@ -2138,7 +2139,7 @@ bool PluginManager::CallPluginItem(DWORD SysID, CallPluginInfo *Data)
 			if (curType!=MODALTYPE_PANELS)
 				return false;
 
-			if (!Data->pPlugin->HasConfigure())
+			if (IsLuamacro ? !Data->pPlugin->HasConfigureV3() : !Data->pPlugin->HasConfigure())
 				return false;
 			break;
 
@@ -2201,20 +2202,45 @@ bool PluginManager::CallPluginItem(DWORD SysID, CallPluginInfo *Data)
 
 		if ((Data->CallFlags & CPT_MASK)==CPT_MENU || (Data->CallFlags & CPT_MASK)==CPT_CONFIGURE)
 		{
+			int Type = (Data->CallFlags & CPT_MASK)==CPT_MENU ? MTYPE_COMMANDSMENU : MTYPE_CONFIGSMENU;
+			const GUID *Guids = MenuItemGuids(Type, &Info);
+
 			auto ItemFound = false;
-			if (Data->ItemUuid == 0) // 0 means "not specified"
+			if (IsLuamacro ? !Data->ItemUuid : !Data->ItemNumber) // 0 means "not specified"
 			{
-				if (MenuItemsCount == 1)
+				if (MenuItemsCount)
 				{
-					Data->FoundUuid = 0;
-					Data->ItemUuid = 0;
+					if (IsLuamacro) {
+						Data->FoundItemNumber = 0;
+						Data->FoundUuid = Guids[0];
+						Data->ItemUuid = &Data->FoundUuid;
+					}
+					else {
+						Data->FoundItemNumber = 0;
+						Data->ItemNumber = 0;
+					}
 					ItemFound = true;
 				}
 			}
-			else if (Data->ItemUuid <= MenuItemsCount)
+			else
 			{
-				Data->FoundUuid = Data->ItemUuid - 1; // 1-based on the user side
-				ItemFound = true;
+				if (!IsLuamacro) {
+					if (Data->ItemNumber <= MenuItemsCount) {
+						Data->FoundItemNumber = Data->ItemNumber - 1; // 1-based on the user side
+						ItemFound = true;
+					}
+				}
+				else {
+					for (int ii=0; ii < MenuItemsCount; ii++) {
+						if (!memcmp(Data->ItemUuid, &Guids[ii], sizeof(GUID))) {
+							Data->FoundUuid = *Data->ItemUuid;
+							Data->ItemUuid = &Data->FoundUuid;
+							Data->FoundItemNumber = ii;
+							ItemFound = true;
+							break;
+						}
+					}
+				}
 			}
 			if (!ItemFound)
 				return false;
@@ -2234,7 +2260,10 @@ bool PluginManager::CallPluginItem(DWORD SysID, CallPluginInfo *Data)
 	case CPT_MENU:
 		{
 			auto OpenCode = OPEN_PLUGINSMENU;
-			INT_PTR Item = Data->FoundUuid;
+			INT_PTR Item = Data->FoundItemNumber;
+			if (IsLuamacro) {
+				Item = reinterpret_cast<INT_PTR>(&Data->FoundUuid);
+			}
 			OpenDlgPluginData pd { sizeof(pd) };
 
 			if (IsEditor)
@@ -2248,7 +2277,12 @@ bool PluginManager::CallPluginItem(DWORD SysID, CallPluginInfo *Data)
 			else if (IsDialog)
 			{
 				OpenCode = OPEN_DIALOG;
-				pd.ItemNumber = Data->FoundUuid;
+				if (!IsLuamacro) {
+					pd.ItemNumber = Data->FoundItemNumber;
+				}
+				else {
+					pd.ItemGuid = Data->FoundUuid;
+				}
 				pd.hDlg = reinterpret_cast<Dialog*>(TopFrame);
 				Item = reinterpret_cast<intptr_t>(&pd);
 			}
@@ -2259,7 +2293,10 @@ bool PluginManager::CallPluginItem(DWORD SysID, CallPluginInfo *Data)
 		break;
 
 	case CPT_CONFIGURE:
-		ConfigureCurrent(Data->pPlugin,Data->FoundUuid);
+		if (IsLuamacro) {
+			UpdateLMInfo();
+		}
+		ConfigureCurrent(Data->pPlugin,Data->FoundItemNumber);
 		return true;
 
 	case CPT_CMDLINE:

far/src/plug/plugins.hpp
@@ -202,12 +202,14 @@ class PluginManager
 			int OpenFrom;
 			union
 			{
-				int ItemUuid;
+				int ItemNumber;
+				GUID *ItemUuid;
 				const wchar_t *Command;
 			};
 			// Используется в функции CallPluginItem для внутренних нужд
 			Plugin *pPlugin;
-			int FoundUuid;
+			GUID FoundUuid;
+			int FoundItemNumber;
 		};
 
 		BitFlags Flags;        // флаги манагера плагинов

luamacro/src/macrotest.lua
@@ -1387,6 +1387,10 @@ end
 
 function MT.test_Plugin()
   assert_false(Plugin.Menu())
+  assert_true(Plugin.Menu(luamacroId, "EF6D67A2-59F7-4DF3-952E-F9049877B492"))
+  assert_true(Area.Menu)
+  Keys("Esc")
+
   assert_false(Plugin.Config())
   assert_false(Plugin.Command())
   assert_true(Plugin.Command(luamacroId))