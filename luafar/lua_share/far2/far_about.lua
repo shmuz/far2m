7ba460bca879acad37a63d40c55fa6a99b2867d5
Shmuel Zeigerman <solomuz0@gmail.com>
2024-08-07 19:22:12 +0300

    Refactor farabout

--- luafar/lua_share/far2/far_about.lua
+++ luafar/lua_share/far2/far_about.lua
@@ -5,8 +5,7 @@ local function FarAbout()
   local Items, Bkeys = {}, { {BreakKey="CtrlC"} }
   local Array = {}
 
-  local Add = function(name, val, skipnil)
-    if skipnil and val==nil then return end
+  local Add = function(name, val)
     name = name or ""
     val = val == nil and "" or tostring(val)
     local text = ("%-30s│ %s"):format(name, val)
@@ -14,44 +13,52 @@ local function FarAbout()
     table.insert(Array, text)
   end
 
+  local AddEnv = function(name, indent)
+    local val = os.getenv(name)
+    if val ~= nil then
+      if indent then name = (" "):rep(indent)..name end
+      Add(name, val)
+    end
+  end
+
   local Inf = Far.GetInfo()
   local un = win.uname()
 
-  Add("FAR2M version",       Inf.Build)
+  Add("FAR2M version",         Inf.Build)
   Add("  Platform",            Inf.Platform)
   Add("  Backend",             Inf.WinPortBackEnd)
   Add("  ConsoleColorPalette", Inf.ConsoleColorPalette)
   Add("  Admin",               Far.IsUserAdmin and "yes" or "no")
   Add("  PID",                 Far.PID)
-  Add("  Main and Help languages", Inf.MainLang..", "..Inf.HelpLang)
-  Add("  OEM and ANSI codepages", win.GetOEMCP()..", "..win.GetACP())
-  Add("  FARHOME", os.getenv("FARHOME"), true)
+  Add("  Main and Help languages", Inf.MainLang  ..", ".. Inf.HelpLang)
+  Add("  OEM and ANSI codepages", win.GetOEMCP() ..", ".. win.GetACP())
+  AddEnv("FARHOME", 2)
   Add("  Config directory", far.InMyConfig())
   Add("  Cache directory",  far.InMyCache())
   Add("  Temp directory",   far.InMyTemp())
 
   Add()
-  Add("uname",               "")
-  Add("  sysname",           un.sysname)
-  Add("  release",           un.release)
-  Add("  version",           un.version)
-  Add("  machine",           un.machine)
+  Add("uname",     "")
+  Add("  sysname", un.sysname)
+  Add("  release", un.release)
+  Add("  version", un.version)
+  Add("  machine", un.machine)
 
   Add()
-  Add("Host",             win.GetHostName())
-  Add("User",             os.getenv("USER"), true)
-  Add("XDG_SESSION_TYPE", os.getenv("XDG_SESSION_TYPE"), true)
-  Add("TERM",             os.getenv("TERM"), true)
-  Add("COLORTERM",        os.getenv("COLORTERM"), true)
-  Add("GDK_BACKEND",      os.getenv("GDK_BACKEND"), true)
-  Add("DESKTOP_SESSION",  os.getenv("DESKTOP_SESSION"), true)
-  Add("WAYLAND_DISPLAY",  os.getenv("WAYLAND_DISPLAY"), true)
-  Add("WSL_DISTRO_NAME",  os.getenv("WSL_DISTRO_NAME"), true)
+  Add("Host", win.GetHostName())
+  AddEnv("COLORTERM")
+  AddEnv("DESKTOP_SESSION")
+  AddEnv("GDK_BACKEND")
+  AddEnv("TERM")
+  AddEnv("USER")
+  AddEnv("WAYLAND_DISPLAY")
+  AddEnv("WSL_DISTRO_NAME")
+  AddEnv("XDG_SESSION_TYPE")
 
   local plugs = far.GetPlugins()
   Add()
   Add("-- Plugins (" ..#plugs.. ")")
-  for i,v in ipairs(plugs) do
+  for _, v in ipairs(plugs) do
    local dt = far.GetPluginInformation(v)
    --Add(dt.ModuleName:match("[^/]+$"), "")
    Add(dt.GInfo.Title, dt.GInfo.Description)
