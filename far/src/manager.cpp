shmuz/far2m

commit 075471a7eb8b615d3049594fe35d8accbdec31d2

Fix screen redraw after calling modal editor or viewer from dialog procedure

far/src/manager.cpp
@@ -313,11 +313,14 @@ void Manager::ExecuteModal(Frame *Executed)
   а глобальные счётчики не концептуально,
   то лучше это делать тут.
 */
-void Manager::ExecuteModalEV()
+void Manager::ExecuteModalEV(bool RefreshScreen)
 {
 	ModalEVCount++;
 	ExecuteModal();
 	ModalEVCount--;
+	if (RefreshScreen) {
+		ProcessKey(KEY_CONSOLE_BUFFER_RESIZE); //redraw all
+	}
 }
 
 /* $ 11.10.2001 IS

far/src/copy.cpp
@@ -2844,8 +2844,7 @@ LONG_PTR WINAPI WarnDlgProc(HANDLE hDlg, int Msg, int Param1, LONG_PTR Param2)
 				Viewer.SetDynamicallyBorn(false);
 				// а этот трюк не даст пользователю сменить текущий каталог по CtrlF10 и этим ввести в заблуждение копир:
 				Viewer.SetFileHolder(std::make_shared<FileHolder>(ViewName, true));
-				FrameManager->ExecuteModalEV();
-				FrameManager->ProcessKey(KEY_CONSOLE_BUFFER_RESIZE);
+				FrameManager->ExecuteModalEV(true);
 			}
 		} break;
 		case DN_CTLCOLORDLGITEM: {

far/src/fileedit.cpp
@@ -2895,7 +2895,7 @@ void EditConsoleHistory(HANDLE con_hnd, bool Modal)
 		DWORD editorExitCode = ShellEditor->GetExitCode();
 		if (editorExitCode != XC_LOADING_INTERRUPTED && editorExitCode != XC_OPEN_ERROR) {
 			if (Modal)
-				FrameManager->ExecuteModalEV();
+				FrameManager->ExecuteModalEV(false);
 			else
 				FrameManager->Commit();
 		}

far/src/fileview.cpp
@@ -609,7 +609,7 @@ void ModalViewFile(const std::string &pathname)
 	FileViewer Viewer(StrMB2Wide(pathname).c_str(),
 		FALSE, FALSE, FALSE, -1, nullptr, nullptr, FALSE, CP_AUTODETECT);
 	Viewer.SetDynamicallyBorn(false);
-	FrameManager->ExecuteModalEV();
+	FrameManager->ExecuteModalEV(false);
 	const int r = Viewer.GetExitCode();
 	if (r != 0)
 		fprintf(stderr, "%s: viewer error %d for '%s'\n", __FUNCTION__, r, pathname.c_str());
@@ -631,7 +631,7 @@ void ViewConsoleHistory(HANDLE con_hnd, bool modal, bool autoclose)
 	if (autoclose)
 		Viewer->SetAutoClose(true);
 	if (modal)
-		FrameManager->ExecuteModalEV();
+		FrameManager->ExecuteModalEV(false);
 	const int r = Viewer->GetExitCode();
 	if (!r || modal)
 		delete Viewer;

far/src/findfile.cpp
@@ -1846,9 +1846,7 @@ static LONG_PTR WINAPI FindDlgProc(HANDLE hDlg, int Msg, int Param1, LONG_PTR Pa
 										ShellViewer.SetSaveToSaveAs(true);
 									}
 								}
-								FrameManager->ExecuteModalEV();
-								// заставляем рефрешиться экран
-								FrameManager->ProcessKey(KEY_CONSOLE_BUFFER_RESIZE);
+								FrameManager->ExecuteModalEV(true);
 							}
 							SendDlgMessage(hDlg, DM_ENABLEREDRAW, TRUE, 0);
 							SendDlgMessage(hDlg, DM_SHOWDIALOG, TRUE, 0);
@@ -1921,7 +1919,7 @@ static LONG_PTR WINAPI FindDlgProc(HANDLE hDlg, int Msg, int Param1, LONG_PTR Pa
 											ShellEditor.SetFileHolder(TFH);
 										}
 									}
-									FrameManager->ExecuteModalEV();
+									FrameManager->ExecuteModalEV(false);
 									if (TFH) {
 										TFH->CheckForChanges();
 									}

far/src/manager.hpp
@@ -110,7 +110,7 @@ class Manager
 		// Запускает немодальный фрейм в модальном режиме
 		void ExecuteNonModal();
 
-		void ExecuteModalEV();
+		void ExecuteModalEV(bool RefreshScreen);
 
 		//  Функции, которые работают с очередью немодального фрейма.
 		//  Сейчас используются только для хранения информаци о наличии запущенных объектов типа VFMenu

far/src/plug/plugapi.cpp
@@ -1819,7 +1819,7 @@ static int FarViewerSynched(const wchar_t *FileName, const wchar_t *Title, int X
 
 		/* $ 28.05.2001 По умолчанию Вьюер, поэтому нужно здесь признак выставиль явно */
 		Viewer.SetDynamicallyBorn(false);
-		FrameManager->ExecuteModalEV();
+		FrameManager->ExecuteModalEV(true);
 
 		ApplyViewerDeleteOnClose(&Viewer, FileName, Flags);
 
@@ -1925,7 +1925,7 @@ int FarEditorSynched(const wchar_t *FileName, const wchar_t *Title, int X1, int 
 			/* $ 15.05.2002 SKV
 			  Зафиксируем вход и выход в/из модального редактора.
 			*/
-			FrameManager->ExecuteModalEV();
+			FrameManager->ExecuteModalEV(true);
 			ExitCode = Editor.GetExitCode();
 
 			if (ExitCode) {

far/src/usermenu.cpp
@@ -633,7 +633,7 @@ int UserMenu::ProcessSingleMenu(const wchar_t *MenuKey, int MenuPos, const wchar
 									nullptr);
 							delete OldTitle;
 							ShellEditor.SetDynamicallyBorn(false);
-							FrameManager->ExecuteModalEV();
+							FrameManager->ExecuteModalEV(false);
 
 							if (!ShellEditor.IsFileChanged()
 									|| (!MenuFile.Open(strMenuFileName, GENERIC_READ, FILE_SHARE_READ,

commit 60318f291ec0f2e554b8f0e499b3cd02ee7fb6a7

Fix screen redraw after closing the modal editor called from a menu
(was broken since commit 075471a7)

far/src/manager.cpp
@@ -319,7 +319,9 @@ void Manager::ExecuteModalEV(bool RefreshScreen)
 	ExecuteModal();
 	ModalEVCount--;
 	if (RefreshScreen) {
-		ProcessKey(KEY_CONSOLE_BUFFER_RESIZE); //redraw all
+		auto top = GetTopModal();
+		if (!(top && top->GetType() == MODALTYPE_VMENU))
+			ProcessKey(KEY_CONSOLE_BUFFER_RESIZE); //redraw all
 	}
 }
 