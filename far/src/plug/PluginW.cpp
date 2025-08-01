shmuz/far2m

commit f71be18be398ec082d9764ff887af7b9663e1ebf

Refactoring

far/src/plug/PluginW.cpp
@@ -449,28 +449,26 @@ static BOOL LoadLuafar()
 
 	// 1. Load Lua
 #ifdef __APPLE__
-	const char *jit[] =
-		{ "libluajit-5.1.dylib", "/opt/homebrew/lib/libluajit-5.1.dylib", nullptr };
-	const char *lua[] =
-		{ "liblua5.1.dylib", "/opt/homebrew/lib/liblua5.1.dylib", nullptr };
+	const char *ext = ".dylib";
+	const char *paths[] = { "", "/opt/homebrew/lib/", nullptr };
 #else
-	const char *jit[] =
-		{ "libluajit-5.1.so", "/usr/local/lib/libluajit-5.1.so", "/usr/lib/libluajit-5.1.so", nullptr };
-	const char *lua[] =
-		{ "liblua5.1.so", "/usr/local/lib/liblua5.1.so", "/usr/lib/liblua5.1.so", nullptr };
+	const char *ext = ".so";
+	const char *paths[] = { "", "/usr/local/lib/", "/usr/lib/", nullptr };
 #endif
 
-	const char **libs[] { jit, lua };
-	auto str = getenv("FARPLAINLUA");
+	const char *names[] = { "libluajit-5.1", "liblua5.1" }; // luajit is first by default
+	const auto str = getenv("FARPLAINLUA");
 	if (str && (0 == strcmp(str, "1") || 0 == strcasecmp(str, "yes")))
-		std::swap(libs[0], libs[1]);
+		std::swap(names[0], names[1]);
 
 	void *handle = nullptr;
-	for (size_t i=0; i < ARRAYSIZE(libs) && !handle; i++)
+	for (size_t i=0; i < ARRAYSIZE(names) && !handle; i++)
 	{
-		for (const char **ptr=libs[i]; *ptr; ptr++)
-			if ((handle = dlopen(*ptr, RTLD_LAZY|RTLD_GLOBAL)))
-				break;
+		for (const char *path=*paths; path && !handle; path++)
+		{
+			const auto strName = std::string(path) + names[i] + ext;
+			handle = dlopen(strName.c_str(), RTLD_LAZY|RTLD_GLOBAL);
+		}
 	}
 	if (!handle)
 	{

far/src/console/scrbuf.cpp
@@ -57,7 +57,7 @@ enum
 // #endif
 
 #ifdef DIRECT_RT
-extern int DirectRT;
+int DirectRT;
 #endif
 
 ScreenBuf ScrBuf;

far/src/main.cpp
@@ -77,10 +77,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "farcolors.hpp"
 #include "message.hpp"
 
-#ifdef DIRECT_RT
-int DirectRT=0;
-#endif
-
 static void print_help(const char *self)
 {
 	printf("FAR2M - dual-panel file manager with built-in terminal\n"
@@ -144,12 +140,11 @@ static FARString ReconstructCommandLine(int argc, char **argv)
 	return cmd;
 }
 
-static void UpdatePathOptions(const FARString &strDestName, bool IsActivePanel)
+static void UpdatePathOptions(const FARString &strDestName, bool IsLeftPanel)
 {
 	FARString *outFolder, *outCurFile;
 
-	// Та панель, которая имеет фокус - активна (начнем по традиции с Левой Панели ;-)
-	if (IsActivePanel == (bool)Opt.LeftPanel.Focus) {
+	if (IsLeftPanel) {
 		Opt.LeftPanel.Type = FILE_PANEL;  // сменим моду панели
 		Opt.LeftPanel.Visible = TRUE;     // и включим ее
 		outFolder = &Opt.strLeftFolder;
@@ -229,7 +224,8 @@ static int MainProcess(
 			if (Opt.OnlyEditorViewerUsed == Options::ONLY_EDITOR
 				|| Opt.OnlyEditorViewerUsed == Options::ONLY_EDITOR_ON_CMDOUT)
 			{
-				FileEditor *ShellEditor=new FileEditor(Opt.strEditViewArg,CP_AUTODETECT,FFILEEDIT_CANNEWFILE|FFILEEDIT_ENABLEF6,StartLine,StartChar);
+				FileEditor *ShellEditor = new FileEditor(Opt.strEditViewArg, CP_AUTODETECT,
+						FFILEEDIT_CANNEWFILE | FFILEEDIT_ENABLEF6, StartLine, StartChar);
 				_tran(SysLog(L"make shelleditor %p",ShellEditor));
 
 				if (!ShellEditor->GetExitCode())  // ????????????
@@ -239,7 +235,7 @@ static int MainProcess(
 			}
 			else
 			{
-				FileViewer *ShellViewer=new FileViewer(Opt.strEditViewArg,FALSE);
+				FileViewer *ShellViewer = new FileViewer(Opt.strEditViewArg, FALSE);
 
 				if (!ShellViewer->GetExitCode())
 				{
@@ -268,10 +264,10 @@ static int MainProcess(
 			// юзая Opt.*
 			if (!strDestName1.IsEmpty())  // активная панель
 			{
-				UpdatePathOptions(strDestName1, true);
+				UpdatePathOptions(strDestName1, Opt.LeftPanel.Focus);
 
 				if (!strDestName2.IsEmpty())  // пассивная панель
-					UpdatePathOptions(strDestName2, false);
+					UpdatePathOptions(strDestName2, !Opt.LeftPanel.Focus);
 			}
 
 			// теперь все готово - создаем панели!

commit 0d0c3e29a4d0e5ffd111ad1dc8dd41ed706f4e1d

Fix a nasty bug in LoadLuafar() introduced in f71be18be

far/src/plug/PluginW.cpp
@@ -549,10 +549,10 @@ static BOOL LoadLuafar()
 	for (size_t i=0; i < ARRAYSIZE(names) && !handle; i++)
 	{
 		fprintf(stderr, "@ %s #1\n", __FUNCTION__);
-		for (const char *path=*paths; path && !handle; path++)
+		for (const char **path=paths; *path && !handle; path++)
 		{
 			fprintf(stderr, "@ %s #2\n", __FUNCTION__);
-			const auto strName = std::string(path) + names[i] + ext;
+			const auto strName = std::string(*path) + names[i] + ext;
 			handle = dlopen(strName.c_str(), RTLD_LAZY|RTLD_GLOBAL);
 			fprintf(stderr, "@ %s #3\n", __FUNCTION__);
 		}