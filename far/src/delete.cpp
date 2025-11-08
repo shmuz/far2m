shmuz/far2m

commit b95af0417bbd1de2033657dfb705a7abf2ea876b

Refactoring

far/src/delete.cpp
@@ -103,7 +103,7 @@ private:
 
 void ShellDelete(Panel *SrcPanel, bool Wipe)
 {
-	SudoClientRegion scr;
+	SCOPED_ACTION(SudoClientRegion);
 	SCOPED_ACTION(TPreRedrawFuncGuard)(PR_ShellDeleteMsg);
 	FAR_FIND_DATA_EX FindData;
 	FARString strDeleteFilesMsg;
@@ -111,17 +111,16 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 	FARString strDizName;
 	FARString strFullName;
 	DWORD FileAttr;
-	int SelCount, UpdateDiz;
-	int DizPresent;
-	int Ret;
-	BOOL NeedUpdate = TRUE, NeedSetUpADir = FALSE;
+	int SelCount;
+	bool UpdateDiz, DizPresent;
+	bool NeedUpdate = true, NeedSetUpADir = false;
 	int Opt_DeleteToRecycleBin = Opt.DeleteToRecycleBin;
 	/*& 31.05.2001 OT Запретить перерисовку текущего фрейма*/
 	Frame *FrameFromLaunched = FrameManager->GetCurrentFrame();
 	FrameFromLaunched->Lock();
 	DeleteAllFolders = !Opt.Confirm.DeleteFolder;
-	UpdateDiz = (Opt.Diz.UpdateMode == DIZ_UPDATE_ALWAYS
-			|| (SrcPanel->IsDizDisplayed() && Opt.Diz.UpdateMode == DIZ_UPDATE_IF_DISPLAYED));
+	UpdateDiz = Opt.Diz.UpdateMode == DIZ_UPDATE_ALWAYS
+			|| (SrcPanel->IsDizDisplayed() && Opt.Diz.UpdateMode == DIZ_UPDATE_IF_DISPLAYED);
 
 	if (!(SelCount = SrcPanel->GetSelCount()))
 		goto done;
@@ -138,7 +137,7 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 		SrcPanel->GetSelNameCompat(&strSelName, FileAttr);
 
 		if (TestParentFolderName(strSelName) || strSelName.IsEmpty()) {
-			NeedUpdate = FALSE;
+			NeedUpdate = false;
 			goto done;
 		}
 
@@ -162,9 +161,7 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 		strDeleteFilesMsg.Format(Msg::AskDeleteItems, SelCount, Ends);
 	}
 
-	Ret = 1;
-
-	if (Ret && (Opt.Confirm.Delete || SelCount > 1))    // || (FileAttr & FILE_ATTRIBUTE_DIRECTORY)))
+	if (Opt.Confirm.Delete || SelCount > 1)    // || (FileAttr & FILE_ATTRIBUTE_DIRECTORY)))
 	{
 		const wchar_t *DelMsg;
 		const wchar_t *TitleMsg = Wipe ? Msg::DeleteWipeTitle : Msg::DeleteTitle;
@@ -209,7 +206,7 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 									: Opt.DeleteToRecycleBin ? Msg::DeleteRecycle
 															 : Msg::Delete),
 					Msg::Cancel)) {
-			NeedUpdate = FALSE;
+			NeedUpdate = false;
 			goto done;
 		}
 	}
@@ -223,7 +220,7 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 		if (Message(MSG_WARNING, 2, MsgId, (Wipe ? Msg::WipeFilesTitle : Msg::DeleteFilesTitle),
 					(Wipe ? Msg::AskWipe : Msg::AskDelete), strDeleteFilesMsg, Msg::DeleteFileAll,
 					Msg::DeleteFileCancel)) {
-			NeedUpdate = FALSE;
+			NeedUpdate = false;
 			goto done;
 		}
 	}
@@ -232,10 +229,10 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 		SrcPanel->ReadDiz();
 
 	SrcPanel->GetDizName(strDizName);
-	DizPresent = (!strDizName.IsEmpty() && apiGetFileAttributes(strDizName) != INVALID_FILE_ATTRIBUTES);
+	DizPresent = !strDizName.IsEmpty() && apiGetFileAttributes(strDizName) != INVALID_FILE_ATTRIBUTES;
 	DeleteTitle = new ConsoleTitle(Msg::DeletingTitle);
 
-	if ((NeedSetUpADir = CheckUpdateAnotherPanel(SrcPanel, strSelName)) == -1)
+	if (!(NeedSetUpADir = CheckUpdateAnotherPanel(SrcPanel, strSelName)))
 		goto done;
 
 	if (SrcPanel->GetType() == TREE_PANEL)
@@ -335,7 +332,7 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 									Msg::DeleteFileSkip, Msg::DeleteFileCancel);
 
 						if (MsgCode < 0 || MsgCode == 3) {
-							NeedSetUpADir = FALSE;
+							NeedSetUpADir = false;
 							break;
 						}
 

far/src/cmdline.cpp
@@ -246,7 +246,7 @@ int CommandLine::ProcessKey(FarKey Key)
 		VTLog::Reset(NULL);
 		ShowBackground();
 		Redraw();
-//		ShellUpdatePanels(CtrlObject->Cp()->ActivePanel, FALSE);
+//		ShellUpdatePanels(CtrlObject->Cp()->ActivePanel, false);
 		CtrlObject->MainKeyBar->Refresh(Opt.ShowKeyBar);
 
 //		CmdExecute(L"reset", true, false, true, false, false, false);

far/src/execute.cpp
@@ -540,7 +540,7 @@ int CommandLine::CmdExecute(const wchar_t *CmdLine, bool SeparateWindow, bool Di
 	}
 
 	if (!Flags.Check(FCMDOBJ_LOCKUPDATEPANEL) && CtrlObject) {
-		ShellUpdatePanels(CtrlObject->Cp()->ActivePanel, FALSE);
+		ShellUpdatePanels(CtrlObject->Cp()->ActivePanel, false);
 		CtrlObject->MainKeyBar->Refresh(Opt.ShowKeyBar);
 	}
 

far/src/flink.cpp
@@ -132,7 +132,7 @@ int WINAPI FarMkLink(const wchar_t *ExistingName, const wchar_t *NewName, DWORD 
 	}
 
 	if (Result && !(Flags&FLINK_DONOTUPDATEPANEL))
-		ShellUpdatePanels(nullptr, FALSE);
+		ShellUpdatePanels(nullptr, false);
 
 	return Result;
 }

far/src/mix/panelmix.cpp
@@ -72,7 +72,7 @@ int GetColumnTypeWidth(unsigned ColIndex)
 	return ColIndex < ARRAYSIZE(ColumnTypes) ? ColumnTypes[ColIndex].Width : 0;
 }
 
-void ShellUpdatePanels(Panel *SrcPanel,BOOL NeedSetUpADir)
+void ShellUpdatePanels(Panel *SrcPanel,bool NeedSetUpADir)
 {
 	if (!SrcPanel)
 		SrcPanel=CtrlObject->Cp()->ActivePanel;
@@ -121,7 +121,7 @@ void ShellUpdatePanels(Panel *SrcPanel,BOOL NeedSetUpADir)
 	CtrlObject->Cp()->Redraw();
 }
 
-int CheckUpdateAnotherPanel(Panel *SrcPanel,const wchar_t *SelName)
+bool CheckUpdateAnotherPanel(Panel *SrcPanel,const wchar_t *SelName)
 {
 	if (!SrcPanel)
 		SrcPanel=CtrlObject->Cp()->ActivePanel;
@@ -141,11 +141,11 @@ int CheckUpdateAnotherPanel(Panel *SrcPanel,const wchar_t *SelName)
 		if (wcsstr(strAnotherCurDir,strFullName))
 		{
 			((FileList*)AnotherPanel)->CloseChangeNotification();
-			return TRUE;
+			return true;
 		}
 	}
 
-	return FALSE;
+	return false;
 }
 
 int _MakePath1(DWORD Key, FARString &strPathName, const wchar_t *Param2, int escaping)

far/src/mix/panelmix.hpp
@@ -36,8 +36,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "panel.hpp"
 #include "panelctype.hpp"
 
-void ShellUpdatePanels(Panel *SrcPanel,BOOL NeedSetUpADir=FALSE);
-int  CheckUpdateAnotherPanel(Panel *SrcPanel,const wchar_t *SelName);
+void ShellUpdatePanels(Panel *SrcPanel, bool NeedSetUpADir = false);
+bool CheckUpdateAnotherPanel(Panel *SrcPanel, const wchar_t *SelName);
 
 int _MakePath1(DWORD Key, FARString &strPathName, const wchar_t *Param2, int escaping = 1); // by default escaping filenames
 

far/src/panels/filelist.cpp
@@ -990,7 +990,7 @@ class FileList_TempFileHolder : public TempFileUploadHolder
 	void CheckPanelUpdate(Panel *panel)
 	{
 		if (panel && panel->GetPluginHandle() == hPlugin) {
-			ShellUpdatePanels(panel, FALSE);
+			ShellUpdatePanels(panel, false);
 		}
 	}
 

far/src/panels/infolist.cpp
@@ -589,7 +589,7 @@ int InfoList::ProcessKey(FarKey Key)
 			int64_t Pos, Length;
 			DWORD Flags;
 			DizView->GetSelectedParam(Pos, Length, Flags);
-			// ShellUpdatePanels(nullptr,FALSE);
+			// ShellUpdatePanels(nullptr,false);
 			DizView->InRecursion++;
 			Redraw();
 			CtrlObject->Cp()->GetAnotherPanel(this)->Redraw();

commit a34868ddfeee79e0fa4a95f8c52129ef11e33af4

Fix deletion and wiping (broken in b95af041)

far/src/delete.cpp
@@ -235,8 +235,8 @@ void ShellDelete(Panel *SrcPanel, bool Wipe)
 	DizPresent = !strDizName.IsEmpty() && apiGetFileAttributes(strDizName) != INVALID_FILE_ATTRIBUTES;
 	DeleteTitle = new ConsoleTitle(Msg::DeletingTitle);
 
-	if (!(NeedSetUpADir = CheckUpdateAnotherPanel(SrcPanel, strSelName)))
-		goto done;
+	//if (!(NeedSetUpADir = CheckUpdateAnotherPanel(SrcPanel, strSelName)))
+	//	goto done;
 
 	if (SrcPanel->GetType() == TREE_PANEL)
 		FarChDir(L"/");