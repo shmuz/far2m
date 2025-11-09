shmuz/far2m

commit 7335fc02a7164c925c172d517c813c977d018ec3

Fix missed dot-slash prefix for Ctrl+Enter (from far2l PR)

far/src/panels/filelist.cpp
@@ -1229,6 +1229,7 @@ int FileList::ProcessKey(FarKey Key)
 		{
 			if (FileCount > 0 && SetCurPath()) {
 				FARString strFileName;
+				bool localPath = true;
 
 				if (Key == KEY_CTRLSHIFTENTER || Key == KEY_CTRLSHIFTNUMENTER) {
 					_MakePath1(Key, strFileName, L" ");
@@ -1252,41 +1253,12 @@ int FileList::ProcessKey(FarKey Key)
 					}
 
 					if (Key == KEY_CTRLF || Key == KEY_CTRLALTF) {
+						// full paths aren't needed to be prefixed with ./
+						localPath = false;
 						if (PanelMode != PLUGIN_PANEL) {
-							CreateFullPathName(CurPtr->strName, CurPtr->FileAttr, strFileName,
-									Key == KEY_CTRLALTF);
-							EnsurePathHasParentPrefix(strFileName);
+							CreateFullPathName(CurPtr->strName, strFileName, Key == KEY_CTRLALTF);
 						} else {
-							// paths from plugins are not related to host's filesystem thus no need to prefix with ./
-							OpenPluginInfo Info = {0};
-							CtrlObject->Plugins.GetOpenPluginInfo(hPlugin, &Info);
-							FARString strFullName;
-							if (Info.CurURL && Info.CurURL[0]) {
-								strFullName = Info.CurURL;
-							} else if (Info.CurDir && Info.CurDir[0]) {
-								strFullName = Info.CurDir;
-							} else {
-								//fprintf(stderr, "Both CurDir and CurURL are empty or null\n");
-							}
-
-							if (!strFullName.IsEmpty())
-								AddEndSlash(strFullName);
-
-							if (Opt.PanelCtrlFRule) {
-								/* $ 13.10.2000 tran
-								  по Ctrl-f имя должно отвечать условиям на панели */
-								if (ViewSettings.FileLowerCase
-										&& !(CurPtr->FileAttr & FILE_ATTRIBUTE_DIRECTORY))
-									strFileName.Lower();
-
-								if (ViewSettings.FileUpperToLowerCase)
-									if (!(CurPtr->FileAttr & FILE_ATTRIBUTE_DIRECTORY)
-											&& !IsCaseMixed(strFileName))
-										strFileName.Lower();
-							}
-
-							strFullName+= strFileName;
-							strFileName = strFullName;
+							PluginGetURL(CurPtr->strName, strFileName);
 						}
 					}
 
@@ -1295,6 +1267,10 @@ int FileList::ProcessKey(FarKey Key)
 
 					if (Opt.QuotedName & QUOTEDNAME_INSERT)
 						EscapeSpace(strFileName);
+
+					if (localPath) {
+						EnsurePathHasParentPrefix(strFileName);
+					}
 
 					strFileName+= L" ";
 				}
@@ -3740,7 +3716,7 @@ void FileList::CopyFiles()
 			if (TestParentFolderName(strSelName)) {
 				strSelName.Truncate(1);
 			}
-			if (!CreateFullPathName(strSelName, FileAttr, strSelName, false)) {
+			if (!CreateFullPathName(strSelName, strSelName, false)) {
 				if (CopyData) {
 					free(CopyData);
 					CopyData = nullptr;
@@ -3801,7 +3777,7 @@ void FileList::CopyNames(bool FullPathName, bool RealName)
 					strQuotedName.Truncate(1);
 				}
 
-				if (!CreateFullPathName(strQuotedName, FileAttr, strQuotedName, RealName)) {
+				if (!CreateFullPathName(strQuotedName, strQuotedName, RealName)) {
 					if (CopyData) {
 						free(CopyData);
 						CopyData = nullptr;
@@ -3812,21 +3788,8 @@ void FileList::CopyNames(bool FullPathName, bool RealName)
 			} else {
 				FARString strFullName = Info.CurDir;
 
-				if (Opt.PanelCtrlFRule && ViewSettings.FolderUpperCase)
-					strFullName.Upper();
-
 				if (!strFullName.IsEmpty())
 					AddEndSlash(strFullName);
-
-				if (Opt.PanelCtrlFRule) {
-					// имя должно отвечать условиям на панели
-					if (ViewSettings.FileLowerCase && !(FileAttr & FILE_ATTRIBUTE_DIRECTORY))
-						strQuotedName.Lower();
-
-					if (ViewSettings.FileUpperToLowerCase)
-						if (!(FileAttr & FILE_ATTRIBUTE_DIRECTORY) && !IsCaseMixed(strQuotedName))
-							strQuotedName.Lower();
-				}
 
 				strFullName+= strQuotedName;
 				strQuotedName = strFullName;
@@ -3868,52 +3831,38 @@ void FileList::CopyNames(bool FullPathName, bool RealName)
 	free(CopyData);
 }
 
-FARString &FileList::CreateFullPathName(const wchar_t *Name, DWORD FileAttr, FARString &strDest, bool RealName)
+FARString &FileList::PluginGetURL(const wchar_t *Name, FARString &strDest)
 {
-	FARString strFileName = strDest;
+	OpenPluginInfo Info = {0};
+	CtrlObject->Plugins.GetOpenPluginInfo(hPlugin, &Info);
+	if (Info.CurURL && Info.CurURL[0]) {
+		strDest = Info.CurURL;
+	} else if (Info.CurDir && Info.CurDir[0]) {
+		strDest = Info.CurDir;
+	} else {
+		//fprintf(stderr, "Both CurDir and CurURL are empty or null\n");
+	}
+
+	if (!strDest.IsEmpty())
+		AddEndSlash(strDest);
+
+	strDest += Name;
+	return strDest;
+}
+
+
+FARString &FileList::CreateFullPathName(const wchar_t *Name, FARString &strDest, bool RealName)
+{
 	const wchar_t *NameLastSlash = LastSlash(Name);
 
 	if (nullptr == NameLastSlash) {
-		ConvertNameToFull(strFileName, strFileName);
+		ConvertNameToFull(Name, strDest);
 	}
 
 	if (Opt.ClassicHotkeyLinkResolving && RealName) {
-		ConvertNameToReal(strFileName, strFileName);
+		ConvertNameToReal(strDest, strDest);
 	}
 
-	// $ 20.10.2000 SVS Сделаем фичу Ctrl-F опциональной!
-	if (Opt.PanelCtrlFRule) {
-		/* $ 13.10.2000 tran
-		  по Ctrl-f имя должно отвечать условиям на панели */
-		if (ViewSettings.FolderUpperCase) {
-			if (FileAttr & FILE_ATTRIBUTE_DIRECTORY) {
-				strFileName.Upper();
-			} else {
-				size_t pos;
-
-				if (FindLastSlash(pos, strFileName))
-					strFileName.Upper(0, pos);
-				else
-					strFileName.Upper();
-			}
-		}
-
-		if (ViewSettings.FileUpperToLowerCase && !(FileAttr & FILE_ATTRIBUTE_DIRECTORY)) {
-			size_t pos;
-
-			if (FindLastSlash(pos, strFileName) && !IsCaseMixed(strFileName.CPtr() + pos))
-				strFileName.Lower(pos);
-		}
-
-		if (ViewSettings.FileLowerCase && !(FileAttr & FILE_ATTRIBUTE_DIRECTORY)) {
-			size_t pos;
-
-			if (FindLastSlash(pos, strFileName))
-				strFileName.Lower(pos);
-		}
-	}
-
-	strDest = strFileName;
 	return strDest;
 }
 

far/src/cfg/ConfigOpt.cpp
@@ -397,7 +397,6 @@ static struct FARConfig
 	{true,  NSecPanel, "CaseSensitiveCompareSelect",    &Opt.PanelCaseSensitiveCompareSelect, 0, OPT_BOOLEAN},
 	{true,  NSecPanel, "ReverseSort",                   &Opt.ReverseSort, 1, OPT_BOOLEAN},
 	{false, NSecPanel, "RightClickRule",                &Opt.PanelRightClickRule, 2, OPT_3STATE},
-	{false, NSecPanel, "CtrlFRule",                     &Opt.PanelCtrlFRule, 1, OPT_BOOLEAN},
 	{false, NSecPanel, "CtrlAltShiftRule",              &Opt.PanelCtrlAltShiftRule, 0, OPT_3STATE},
 	{false, NSecPanel, "RememberLogicalDrives",         &Opt.RememberLogicalDrives, 0, OPT_BOOLEAN},
 	{true,  NSecPanel, "AutoUpdateLimit",               &Opt.AutoUpdateLimit, 0},

far/src/cfg/config.hpp
@@ -484,8 +484,6 @@ struct Options
 	int ClassicHotkeyLinkResolving;
 	int PanelRightClickRule; // задает поведение правой клавиши мыши
 	int PanelCtrlAltShiftRule; // задает поведение Ctrl-Alt-Shift для панелей.
-	// Panel/CtrlFRule в реестре - задает поведение Ctrl-F. Если = 0, то штампуется файл как есть, иначе - с учетом отображения на панели
-	int PanelCtrlFRule;
 	/*
 	  битовые флаги, задают поведение Ctrl-Alt-Shift
 	   бит установлен - функция включена:

far/src/mix/panelmix.cpp
@@ -208,8 +208,7 @@ int _MakePath1(DWORD Key, FARString &strPathName, const wchar_t *Param2, int esc
 					if (NeedRealName && SrcPanel->GetType() == FILE_PANEL
 							&& SrcPanel->GetMode() != PLUGIN_PANEL) {
 						FileList *SrcFilePanel = (FileList *)SrcPanel;
-						SrcFilePanel->CreateFullPathName(strPathName, FILE_ATTRIBUTE_DIRECTORY, strPathName,
-								true);
+						SrcFilePanel->CreateFullPathName(strPathName, strPathName, true);
 					}
 
 					AddEndSlash(strPathName);

far/src/panels/filelist.hpp
@@ -358,7 +358,8 @@ public:
 	int PluginPanelHelp(PHPTR ph);
 	virtual long GetFileCount() const { return FileCount; }
 
-	FARString &CreateFullPathName(const wchar_t *Name, DWORD FileAttr, FARString &strDest, bool RealName);
+	FARString &CreateFullPathName(const wchar_t *Name, FARString &strDest, bool RealName);
+	FARString &PluginGetURL(const wchar_t *Name, FARString &strDest);
 
 	const FileListItem *GetItem(int Index) const;
 	virtual BOOL UpdateKeyBar();

luafar/lua_share/far2/test/macrotest.lua
@@ -902,7 +902,6 @@ local function test_Far_GetConfig()
     "Panel.CaseSensitiveCompareSelect",
     "Panel.ClassicHotkeyLinkResolving",
     "Panel.CtrlAltShiftRule",
-    "Panel.CtrlFRule",
     "Panel.FilenameMarksAlign",
     "Panel.Highlight",
     "Panel/Layout.ColumnTitles",

commit 47b8e559d2ee15a9c9ad4c4d9d585d0492d1b969

Fix Ctrl+F pressed on '..' line (broken since 7335fc02)

far/src/panels/filelist.cpp
@@ -1256,7 +1256,8 @@ int FileList::ProcessKey(FarKey Key)
 						// full paths aren't needed to be prefixed with ./
 						localPath = false;
 						if (PanelMode != PLUGIN_PANEL) {
-							CreateFullPathName(CurPtr->strName, strFileName, Key == KEY_CTRLALTF);
+							CreateFullPathName((CurPtr->strName == L".." ? L"." : CurPtr->strName),
+									strFileName, Key == KEY_CTRLALTF);
 						} else {
 							PluginGetURL(CurPtr->strName, strFileName);
 						}